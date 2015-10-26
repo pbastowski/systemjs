e1baf829a111bb2224bbe85737c93b328c6c9f9e
Paul Bastowski <pbastowski@gmail.com>
2015-10-26 23:03:56 +0100

    v0.0.1 Initial commit.

---
+++ README.md
@@ -0,0 +1,10 @@
+## SystemJS for Meteor 1.2
+
+This package implements SystemJS 0.19.5. It will only work with Meteor 1.2.
+
+### Install
+
+```bash
+meteor install pbastowski:systemjs
+```
+
---
+++ package.js
@@ -0,0 +1,21 @@
+Package.describe({
+    name:          'pbastowski:systemjs',
+    version:       '0.0.1',
+    summary:       'SystemJS for Meteor 1.2',
+    git:           'https://github.com/pbastowski/typescript.git',
+    documentation: 'README.md'
+});
+
+Npm.depends({
+   'systemjs': '0.19.5'
+});
+
+Package.onUse(function (api) {
+    api.versionsFrom('1.1.0.2');
+
+    api.addFiles([
+        '.npm/package/node_modules/systemjs/dist/system.js',
+        '.npm/package/node_modules/systemjs/dist/system-polyfills.js',
+    ], ['client'], { transpile: false} );
+});
+
